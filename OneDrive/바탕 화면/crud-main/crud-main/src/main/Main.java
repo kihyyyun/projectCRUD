406698fe287e647c816be0a71c5ad11d8aa4cee6
Ggyeon <okokh327@gmail.com>
2023-11-30 16:58:24 +0900

    feature D

--- OneDrive/바탕 화면/crud-main/crud-main/src/main/Main.java
+++ OneDrive/바탕 화면/crud-main/crud-main/src/main/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("git push test");
-        System.out.println("pushpushpushpushpushpushpush");
-        System.out.println("kkkkkkk");
         System.out.println("feature-d");
     }
 }
