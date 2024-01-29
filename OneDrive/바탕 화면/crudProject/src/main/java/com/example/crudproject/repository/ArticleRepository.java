709dff5bcbf50d2a2eddd29c4c5dbcd3ce763196
kihyyyun <okokh327@gmail.com>
2024-01-29 19:42:54 +0900

    댓글 및 게시판 기능

--- OneDrive/바탕 화면/crudProject/src/main/java/com/example/crudproject/repository/ArticleRepository.java
+++ OneDrive/바탕 화면/crudProject/src/main/java/com/example/crudproject/repository/ArticleRepository.java
@@ -4,10 +4,11 @@ import com.example.crudproject.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByTitleContaining(String title);
+    List<Article> findByContentContaining(String content);
+
 }
